a2d7e8aad776cc88dc279c70c643831ce1cae909
Daninsyah Bagas Abiyansa <63465768+MayorBee404@users.noreply.github.com>
2023-06-10 18:46:29 +0700

    adding song service

--- src/services/postgress/SongsServices.js
+++ src/services/postgress/SongsServices.js
@@ -31,4 +31,54 @@ class SongsServices {
 
     return result.rows[0].id;
   }
+
+  async getSongs() {
+    const result = await this._pool.query('SELECT * FROM songs');
+    return result.rows;
+  }
+
+  async getSongById(id) {
+    const query = {
+      text: 'SELECT * FROM songs WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Lagu tidak ditemukan');
+    }
+
+    return result.rows.map(mapSongs)[0];
+  }
+
+  async editSongById(id, {
+    title, year, genre, performer, duration, albumId,
+  }) {
+    const query = {
+      text: 'UPDATE songs SET title = $1, body = $2, tags = $3, year = $4, genre = $5, performer = $5, duration = $6, album_id = $7 WHERE id = $8 RETURNING id',
+      values: [title, year, genre, performer, duration, albumId, id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Gagal memperbarui gagal. Id tidak ditemukan');
+    }
+  }
+
+  async deleteSongById(id) {
+    const query = {
+      text: 'DELETE FROM songs WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Lagu gagal dihapus. Id tidak ditemukan');
+    }
+  }
 }
+
+module.exports = SongsServices;
