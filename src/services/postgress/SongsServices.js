95deddf652224d6034af1003d015d43a66423b84
Daninsyah Bagas Abiyansa <63465768+MayorBee404@users.noreply.github.com>
2023-06-10 13:05:56 +0700

    adding Songs Service

---
+++ src/services/postgress/SongsServices.js
@@ -0,0 +1,34 @@
+const Pool = require('pg');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
+const { mapSongs } = require('../../utils');
+
+class SongsServices {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addSong({
+    title,
+    year,
+    genre,
+    performer,
+    duration,
+    albumId,
+  }) {
+    const id = `song-${nanoid(16)}`;
+    const query = {
+      text: 'INSERT INTO songs VALUES($1, $2 ,$3, $4, $5, $6) RETURNING id',
+      values: [id, title, year, genre, performer, duration, albumId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Song gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+}
